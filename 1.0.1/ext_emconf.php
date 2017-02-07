89c6e7f9ff1e8344318ae2007368491949ffcca3
Simon Schmidt <sfs@marketing-factory.de>
2017-02-07 10:30:56 +0000

    create new version

---
+++ 1.0.1/ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "mfc_dataprotection".
+ *
+ * Auto generated 06-01-2014 12:09
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Data Protection Google Analytics Opt-out Cookie',
+	'description' => '',
+	'category' => 'Marketing Factory',
+	'author' => 'Simon Schmidt',
+	'author_email' => 'typo3@marketing-factory.de',
+	'shy' => '',
+	'dependencies' => 'cms',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => 'Marketing Factory Consulting GmbH',
+	'version' => '1.0.1',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '5.3.0-',
+			'typo3' => '6.2.0-7.6.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => '',
+);
