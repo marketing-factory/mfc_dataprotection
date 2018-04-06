3ce720bffc19ceb0cfcc4ab576cb3fc4d069bbc8
Simon Schmidt <sfs@marketing-factory.de>
2018-04-06 16:04:17 +0200

    add facebook opt out possibility

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -9,3 +9,13 @@
     'list_type',
     'mfc_dataprotection'
 );
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+    [
+        'LLL:EXT:mfc_dataprotection/Resources/Private/Language/locallang_db.xlf:CType.fb_opt.header',
+        'mfc_dataprotection_fb_optout',
+        'EXT:mfc_dataprotection/ext_icon.gif'
+    ],
+    'list_type',
+    'mfc_dataprotection'
+);
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 $EM_CONF['mfc_dataprotection'] = [
-    'title' => 'Data Protection Google Analytics Opt-out Cookie',
+    'title' => 'Data Protection Google Analytics  & Facebook Opt-out Cookie',
     'category' => 'Marketing Factory',
     'author' => 'Simon Schmidt',
     'author_email' => 'typo3@marketing-factory.de',
     'state' => 'stable',
     'author_company' => 'Marketing Factory Consulting GmbH',
-    'version' => '2.0.0',
+    'version' => '2.1.0',
     'constraints' => [
         'depends' => [
             'typo3' => '7.6.0-8.7.99',
