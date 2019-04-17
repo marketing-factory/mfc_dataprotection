e49524d53a8eb2b85c8f7fa44ddc93f23d7a115b
Simon Schmidt <sfs@marketing-factory.de>
2019-04-17 14:28:17 +0200

    add description

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,6 +2,7 @@
 
 $EM_CONF['mfc_dataprotection'] = [
     'title' => 'Data Protection Google Analytics  & Facebook Opt-out Cookie',
+    'description' => 'Add the possibility to add a opt-out link for GA and Facebook',
     'category' => 'Marketing Factory',
     'author' => 'Simon Schmidt',
     'author_email' => 'typo3@marketing-factory.de',
